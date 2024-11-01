2550bbb3ee18fafab45066164d267b2a197372d0
h-rsh-n <harshanp1514@gmail.com>
2024-11-01 22:05:50 +0530

    Added idempotent api's:

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,9 +1,11 @@
 const { StatusCodes } = require("http-status-codes");
 const { BookingService } = require("../service");
 const {successMessage,errorMessage} = require('../utils/common')
+const inMemoryDB = {}
 
 async function createBooking(req,res){
   try {
+    console.log(req)
     const booking = await BookingService.createBooking({
       flightId:req.body.flightId,
       userId:req.body.userId,
@@ -21,11 +23,21 @@ async function createBooking(req,res){
 
 async function makePayment(req,res) {
   try {
+    const idempotencyKey = req.headers['x-idempotency-key'];
+    if(!idempotencyKey){
+      errorMessage.error = `Missing idempotency key`
+      return res.status(StatusCodes.BAD_REQUEST).json(errorMessage);
+    }
+    if(inMemoryDB[idempotencyKey] = idempotencyKey){
+      errorMessage.error = `Cannot retry on a successful payment`
+      return res.status(StatusCodes.BAD_REQUEST).json(errorMessage)
+    }
     const payment = await BookingService.makePayment({
       userId:req.body.userId,
       bookingId:req.body.bookingId,
       cost:req.body.cost
     })
+    inMemoryDB[idempotencyKey] = idempotencyKey;
     successMessage.data = payment;
     return res.status(StatusCodes.OK).json(successMessage);
   } catch (error) {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const {serverConfig,loggerConfig} = require('./config')
+const amqp = require('amqplib');
 const app = express();
 const apiRouter = require('./routes')
 const CRON = require('./utils/common/cron')
 
+async function queueConnect(){
+  try {
+    const connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+    channel.assertQueue('noti_queue');
+    setInterval(()=>{
+      channel.sendToQueue('noti_queue', Buffer.from('hello world1'));
+    },1000)
+  } catch (error) {
+    console.log(error)
+  }
+}
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
@@ -12,4 +25,5 @@ app.use('/api',apiRouter);
 app.listen(serverConfig.PORT,()=>{
   console.log(`Server started on port ${serverConfig.PORT}`);
   CRON.scheduleCornJob();
+  queueConnect();
 })
